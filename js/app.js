2d53e436f21169a0ca5fb465adb1ebeb542bfedc
estydaven <esetnod1111@gmail.com>
2024-07-15 17:24:04 +0300

    init project

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// SLIDER
+$('.slider-services__inner').slick({
+    infinite: true,
+    dots: false,
+    prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+    nextArrow: '<button type="button" class="slick-next">Next</button>'
+});
