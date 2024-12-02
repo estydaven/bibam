720c47ebab9bfa809cd696a9f9fd28e57225b419
estydaven <esetnod1111@gmail.com>
2024-12-02 15:42:52 +0300

    changes

--- js/accordeon.js
+++ js/accordeon.js
@@ -3,6 +3,8 @@ accordeonBlocks.forEach(function(block){
     const accordeonBtn = block.querySelector('.show__button');
     const accordeonInfo = block.querySelector('.show-close');
     const accordeonBtnUp = block.querySelector('.up');
+    const sliderFairy = block.querySelector('.gallery-fairytale');
+    const sliderOffice = block.querySelector('.gallery-office');
 
     block.addEventListener('click', function(event) {
         if (!event.target.closest('.up')) {
@@ -11,8 +13,83 @@ accordeonBlocks.forEach(function(block){
             accordeonInfo.classList.remove('hidden');
             block.classList.add('visible');
             block.classList.remove('show_pdn');
+
+            // Проверяем, инициализирован ли слайдер, и если нет, то инициализируем
+            if (sliderFairy && !$(sliderFairy).hasClass('slick-initialized')) {
+                $(sliderFairy).slick({
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                    arrows: false,
+                    dotsClass: 'slick-dots reviews-slider__dots reviews-slider__dots_show',
+                    responsive: [
+                        {
+                            breakpoint: 1024,
+                            settings: {
+                                slidesToShow: 3,
+                                slidesToScroll: 1,
+                            }
+                        },
+                        {
+                            breakpoint: 768,
+                            settings: {
+                                slidesToShow: 2,
+                                slidesToScroll: 1,
+                            }
+                        },
+                        {
+                            breakpoint: 480,
+                            settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1,
+                            }
+                        }
+                    ]
+                });
+            }
+            if (sliderOffice && !$(sliderOffice).hasClass('slick-initialized')) {
+                $(sliderOffice).slick({
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                    arrows: false,
+                    dotsClass: 'slick-dots reviews-slider__dots reviews-slider__dots_show',
+                    responsive: [
+                        {
+                            breakpoint: 1024,
+                            settings: {
+                                slidesToShow: 3,
+                                slidesToScroll: 1,
+                            }
+                        },
+                        {
+                            breakpoint: 768,
+                            settings: {
+                                slidesToShow: 2,
+                                slidesToScroll: 1,
+                            }
+                        },
+                        {
+                            breakpoint: 480,
+                            settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1,
+                            }
+                        }
+                    ]
+                });
+            }
+            // После того как блок раскрылся, пересчитываем позиции слайдера
+            if (sliderFairy) {
+                $(sliderFairy).slick('setPosition');
+            }
+            if (sliderOffice) {
+                $(sliderOffice).slick('setPosition');
+            }
         }
-    })
+    });
 
     if (accordeonBtnUp) {
         accordeonBtnUp.addEventListener('click', function(event) {
--- js/slider.js
+++ js/slider.js
@@ -100,38 +100,39 @@ $('.gallery-walkers').slick({
           }
       ]
 });
-$('.gallery-show').slick({
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    infinite: true,
-    dots: true,
-    arrows: false,
-    dotsClass: 'slick-dots reviews-slider__dots reviews-slider__dots_show',
-    responsive: [
-        {
-            breakpoint: 2000,
-            settings: 'unslick'
-        },
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-          }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1,
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-            }
-          }
-      ]
-});
+
+// $('.gallery-show').slick({
+//     slidesToShow: 2,
+//     slidesToScroll: 1,
+//     infinite: true,
+//     dots: true,
+//     arrows: false,
+//     dotsClass: 'slick-dots reviews-slider__dots reviews-slider__dots_show',
+//     responsive: [
+//         {
+//             breakpoint: 2000,
+//             settings: 'unslick'
+//         },
+//         {
+//           breakpoint: 1024,
+//           settings: {
+//             slidesToShow: 3,
+//             slidesToScroll: 1,
+//           }
+//         },
+//         {
+//             breakpoint: 768,
+//             settings: {
+//               slidesToShow: 2,
+//               slidesToScroll: 1,
+//             }
+//           },
+//           {
+//             breakpoint: 480,
+//             settings: {
+//               slidesToShow: 1,
+//               slidesToScroll: 1,
+//             }
+//           }
+//       ]
+// });
